bf139e6af4ba95d0de50b884406017eb4dfcccae
koheitech <koheitechigh@gmail.com>
2023-03-29 05:13:22 +0300

    add temp task data

--- src/App.vue
+++ src/App.vue
@@ -10,6 +10,33 @@ import Header from "./components/Header.vue";
 export default {
   name: "App",
   components: { Header },
+  data() {
+    return {
+      tasks: [],
+    };
+  },
+  created() {
+    this.tasks = [
+      {
+        id: 1,
+        text: "Doctor appointment",
+        day: "March 1st at 2:30pm",
+        reminder: true,
+      },
+      {
+        id: 2,
+        text: "Meeting at School",
+        day: "March 3rd at 1:30pm",
+        reminder: true,
+      },
+      {
+        id: 3,
+        text: "Food Shopping",
+        day: "March 3rd at 11:30am",
+        reminder: false,
+      },
+    ];
+  },
 };
 </script>
 
