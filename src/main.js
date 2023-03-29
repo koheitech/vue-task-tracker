c1c2f82bbe07026daf680a8b32de2288d8260840
koheitech <koheitechigh@gmail.com>
2023-03-29 18:24:36 +0300

    create router with vue-route

--- src/main.js
+++ src/main.js
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import router from './router'
 
 // entrypoint of vue
-createApp(App).mount('#app')
+createApp(App)
+    .use(router)
+    .mount('#app')
